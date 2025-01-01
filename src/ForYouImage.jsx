c1b7b1f8535345c97878bbb1d51f5179ede03b28
Zuhaib Zulfiqar <zuhaib.zul@gmail.com>
2025-01-01 11:17:57 +0500

    3

--- src/ForYouImage.jsx
+++ src/ForYouImage.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const ForYouImage = () => {
   return (
-    <div className="z-20 my-10 max-w-4xl mx-auto  sm:px-4 lg:px-8">
+    <div className="z-20 my-10 mx-auto px-4 sm:px-6 lg:px-8">
       <img
         src="/for you.png"
         alt="Video Editing Bundle Preview"
-        className="object-cover w-full h-auto rounded-lg shadow-lg"
-        style={{ maxWidth: '500px' }}
+        className="object-cover w-full h-auto rounded-lg shadow-lg sm:max-w-md md:max-w-lg lg:max-w-2xl"
       />
     </div>
   );
