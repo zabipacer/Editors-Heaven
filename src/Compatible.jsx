1a46147937a6a25681e73781b365e9fd4ea1e43e
zabipacer <128690125+zabipacer@users.noreply.github.com>
2025-01-05 14:05:58 +0500

    Update Compatible.jsx

--- src/Compatible.jsx
+++ src/Compatible.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Compatible = () => {
   return (
-    <div className="z-20 mt-10 max-w-4xl">
+    <div className=" mt-10 max-w-4xl">
     <img
       src="/wide.png"
       alt="Video Editing Bundle Preview"
